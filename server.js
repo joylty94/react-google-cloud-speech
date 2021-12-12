46f1437b8aa0ffaa262b330a15aa2348e1605efb
joylty94 <joylty94@naver.com>
2021-12-12 17:07:28 +0900

    수정

--- server.js
+++ server.js
@@ -1,49 +1,87 @@
-const express = require('express')
-const app = express()
-const port = 3001
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const speech = require('@google-cloud/speech')
-const client = new speech.SpeechClient()
+const express = require('express');
+const app = express();
+const port = 3001;
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const recorder = require('node-record-lpcm16');
+const speech = require('@google-cloud/speech');
+const speechClient = new speech.SpeechClient();
+
+const encoding = 'LINEAR16';
+const sampleRateHertz = 16000;
+const languageCode = 'ko-KR'; //en-US
 
 // Configure Transcription Request
 const request = {
-  config: {
-    encoding: 'LINEAR16',
-    sampleRateHertz: 16000,
-    languageCode: 'en-US'
-  },
-  interimResults: true
-}
+	config: {
+		encoding: encoding,
+		sampleRateHertz: sampleRateHertz,
+		languageCode: languageCode,
+	},
+	interimResults: true,
+};
 
-let recognizeStream = null
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-server.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+server.listen(port, () =>
+	console.log(`Example app listening at http://localhost:${port}`)
+);
 
 io.on('connection', (socket) => {
-  console.log('new client connected')
+	console.log('new client connected');
+	let recognizeStream = null;
 
-  socket.on('stream', data => {
-    if (!recognizeStream) {
-      console.log('INIT GOOGLE CLOUD SPEECH')
-      // Create Stream to the Google Speech to Text API
-      recognizeStream = client
-      .streamingRecognize(request)
-      .on('error', console.error)
-      .on('data', data => {
-        console.log(data.results[0].alternatives[0].transcript)
-        socket.emit('data', data.results[0].alternatives[0].transcript)
-      })
-    }
+	socket.on('join', function () {
+		socket.emit('messages', 'Socket Connected to Server');
+	});
 
-    recognizeStream.write(data)
-  })
+	socket.on('messages', function (data) {
+		socket.emit('broad', data);
+	});
 
-  socket.on('stop', () => {
-    console.log('STOP STREAMING')
-    recognizeStream.destroy()
-    recognizeStream = null
-  })
-})
+	socket.on('startGoogleCloudStream', function (data) {
+		console.log('>?????', data);
+		startRecognitionStream(this, data);
+	});
+
+	socket.on('endGoogleCloudStream', function () {
+		stopRecognitionStream();
+	});
+
+	socket.on('binaryData', function (data) {
+		// console.log(data); //log binary data
+		if (recognizeStream !== null) {
+			recognizeStream.write(data);
+		}
+	});
+
+	function startRecognitionStream(client, data) {
+		recognizeStream = speechClient
+			.streamingRecognize(request)
+			.on('error', console.error)
+			.on('data', (data) => {
+				process.stdout.write(
+					data.results[0] && data.results[0].alternatives[0]
+						? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+						: `\n\nReached transcription time limit, press Ctrl+C\n`
+				);
+
+				client.emit('speechData', data);
+
+				// if end of utterance, let's restart stream
+				// this is a small hack. After 65 seconds of silence, the stream will still throw an error for speech length limit
+				if (data.results[0] && data.results[0].isFinal) {
+					stopRecognitionStream();
+					startRecognitionStream(client);
+					// console.log('restarted stream serverside');
+				}
+			});
+	}
+
+	function stopRecognitionStream() {
+		if (recognizeStream) {
+			recognizeStream.end();
+		}
+		recognizeStream = null;
+	}
+});
--- src/App.js
+++ src/App.js
@@ -1,134 +1,166 @@
-import React, { useRef, useEffect, useState } from 'react'
-import './App.css'
-import io from 'socket.io-client'
-import logo from './logo.svg'
+import React, { useRef, useEffect, useState } from 'react';
+import './App.css';
+import io from 'socket.io-client';
+import logo from './logo.svg';
 
 let bufferSize = 2048,
 	context,
 	processor,
 	input,
-	globalStream
+	globalStream;
 
 const downsampleBuffer = (buffer, sampleRate, outSampleRate) => {
 	if (outSampleRate === sampleRate) {
-		return buffer
+		return buffer;
 	}
 	if (outSampleRate > sampleRate) {
-		throw new Error('downsampling rate show be smaller than original sample rate')
+		throw new Error(
+			'downsampling rate show be smaller than original sample rate'
+		);
 	}
-	var sampleRateRatio = sampleRate / outSampleRate
-	var newLength = Math.round(buffer.length / sampleRateRatio)
-	var result = new Int16Array(newLength)
-	var offsetResult = 0
-	var offsetBuffer = 0
+	var sampleRateRatio = sampleRate / outSampleRate;
+	var newLength = Math.round(buffer.length / sampleRateRatio);
+	var result = new Int16Array(newLength);
+	var offsetResult = 0;
+	var offsetBuffer = 0;
 	while (offsetResult < result.length) {
-		var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio)
-		var accum = 0, count = 0
+		var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
+		var accum = 0,
+			count = 0;
 		for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
-			accum += buffer[i]
-			count++
+			accum += buffer[i];
+			count++;
 		}
 
-		result[offsetResult] = Math.min(1, accum / count) * 0x7FFF
-		offsetResult++
-		offsetBuffer = nextOffsetBuffer
+		result[offsetResult] = Math.min(1, accum / count) * 0x7fff;
+		offsetResult++;
+		offsetBuffer = nextOffsetBuffer;
 	}
-	return result.buffer
-}
-
-
+	return result.buffer;
+};
 
 function App() {
-  const socket = useRef(null)
-  const [transcript, setTranscript] = useState('')
-  const [isStreaming, setIsStreaming] = useState(false)
+	const socket = useRef(null);
+	const [transcript, setTranscript] = useState('');
+	const [isStreaming, setIsStreaming] = useState(false);
 
-  useEffect(() => {
-    setupSocket()
-  }, [])
+	useEffect(() => {
+		setupSocket();
+	}, []);
 
-  const setupSocket = () => {
-    socket.current = io('http://localhost:3001', {
-      reconnection: true,
-      reconnectionDelay: 1000,
-      reconnectionDelayMax: 5000,
-      reconnectionAttempts: Infinity,
-    })
+	const setupSocket = () => {
+		socket.current = io('http://localhost:3080', {
+			reconnection: true,
+			reconnectionDelay: 1000,
+			reconnectionDelayMax: 5000,
+			reconnectionAttempts: Infinity,
+		});
 
-    socket.current.on('connect', () => {
-      console.log('connected')
-    })
+		socket.current.on('connect', () => {
+			console.log('connected');
+			socket.current.emit('join', 'Server Connected to Client');
+		});
 
-    socket.current.on('data', (data) => {
-      setTranscript(data)
-    })
-  }
+		socket.current.on('messages', function (data) {
+			console.log(data);
+		});
 
-  const setup = async () => {
-    context = new (window.AudioContext || window.webkitAudioContext)({
-      // if Non-interactive, use 'playback' or 'balanced' // https://developer.mozilla.org/en-US/docs/Web/API/AudioContextLatencyCategory
-      latencyHint: 'interactive',
-    })
-    processor = context.createScriptProcessor(bufferSize, 1, 1)
-    processor.connect(context.destination)
-    context.resume()
+		socket.current.on('speechData', function (data) {
+			// console.log(data.results[0].alternatives[0].transcript);
+			// let dataFinal = undefined || data.results[0].isFinal;
+			console.log('data>>>>>>', data.results);
+			// if (dataFinal === true) {
+			// }
+			setTranscript(data.results[0].alternatives[0].transcript);
+		});
+	};
 
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
-    globalStream = stream
-    input = context.createMediaStreamSource(stream)
-    input.connect(processor)
+	// const setup = async () => {
+	// 	context = new (window.AudioContext || window.webkitAudioContext)({
+	// 		// if Non-interactive, use 'playback' or 'balanced' // https://developer.mozilla.org/en-US/docs/Web/API/AudioContextLatencyCategory
+	// 		latencyHint: 'interactive',
+	// 	});
+	// 	processor = context.createScriptProcessor(bufferSize, 1, 1);
+	// 	processor.connect(context.destination);
+	// 	context.resume();
 
-    processor.onaudioprocess = (e) => {
-      microphoneProcess(e)
-    }
-  }
+	// 	const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+	// 	globalStream = stream;
+	// 	input = context.createMediaStreamSource(stream);
+	// 	input.connect(processor);
 
-  const microphoneProcess = (e) => {
-    var left = e.inputBuffer.getChannelData(0)
-    var left16 = downsampleBuffer(left, 44100, 16000)
-    socket.current.emit('stream', left16)
-  }
+	// 	processor.onaudioprocess = (e) => {
+	// 		microphoneProcess(e);
+	// 	};
+	// };
 
-  const start = () => {
-    setup()
-    setIsStreaming(true)
-  }
+	const microphoneProcess = (e) => {
+		var left = e.inputBuffer.getChannelData(0);
+		var left16 = downsampleBuffer(left, 44100, 16000);
+		socket.current.emit('binaryData', left16);
+	};
 
-  const stop = () => {
-    if (isStreaming) {
-      // stop record track from browser
-      let track = globalStream.getTracks()[0]
-      track.stop()
+	const start = () => {
+		socket.current.emit('startGoogleCloudStream', ''); //init socket Google Speech Connection
 
-      // stop recorder from browser
-      input.disconnect(processor)
-      processor.disconnect(context.destination)
-      context.close().then(() => {
-        input = null
-        processor = null
-        context = null
-        socket.current.emit('stop')
-      })
+		AudioContext = window.AudioContext || window.webkitAudioContext;
+		context = new AudioContext({
+			// if Non-interactive, use 'playback' or 'balanced' // https://developer.mozilla.org/en-US/docs/Web/API/AudioContextLatencyCategory
+			latencyHint: 'interactive',
+		});
+		processor = context.createScriptProcessor(bufferSize, 1, 1);
+		processor.connect(context.destination);
+		context.resume();
 
-      setIsStreaming(false)
-    }
-  }
-  
-  return (
-    <div className="App">
-      <div>
-        <img src={logo} className="App-logo" alt="logo" />
-      </div>
-      <h1>React-Node-Google Cloud Speech Demo</h1>
-      <div>
-        <button onClick={start}>Record</button>
-        <button onClick={stop}>Stop</button>
-      </div>
-      <div>
-        <textarea value={transcript} readOnly={true} rows="10"></textarea>
-      </div>
-    </div>
-  )
+		var handleSuccess = function (stream) {
+			globalStream = stream;
+			input = context.createMediaStreamSource(stream);
+			input.connect(processor);
+
+			processor.onaudioprocess = function (e) {
+				microphoneProcess(e);
+			};
+		};
+
+		navigator.mediaDevices.getUserMedia({ audio: true }).then(handleSuccess);
+		setIsStreaming(true);
+	};
+
+	const stop = () => {
+		if (isStreaming) {
+			// stop record track from browser
+			let track = globalStream.getTracks()[0];
+			track.stop();
+
+			// stop recorder from browser
+			input.disconnect(processor);
+			processor.disconnect(context.destination);
+			context.close().then(() => {
+				input = null;
+				processor = null;
+				context = null;
+				socket.current.emit('endGoogleCloudStream', '');
+			});
+
+			setIsStreaming(false);
+		}
+	};
+
+	return (
+		<div className='App'>
+			<div>
+				<img src={logo} className='App-logo' alt='logo' />
+			</div>
+			<h1>React-Node-Google Cloud Speech Demo</h1>
+			<div>
+				<button onClick={start}>Record</button>
+				<button onClick={stop}>Stop</button>
+			</div>
+			<div>
+				<textarea value={transcript} readOnly={true} rows='10'></textarea>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
